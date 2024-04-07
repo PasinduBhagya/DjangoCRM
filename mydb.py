e435001815e8c0f48ee0963aba0520d0f3c9e5c2
PasinduBhagya <pasindubhagyapeiries@gmail.com>
2024-04-07 07:45:15 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = "localhost",
+    user = "dcrm",
+    passwd = 'Abc@12345',
+    auth_plugin='mysql_native_password'
+)
+
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE temp")
+
+print("All Done")
